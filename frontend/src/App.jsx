d766b30243b2045a23954cf7428fcaab5171b567
sohit kumar <sohitkumar9548@gmail.com>
2026-09-11 23:51:18 +0530

    implemented regsiter and login page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,14 @@
+import { Routes, Route } from "react-router";
+import Login from "./features/auth/pages/Login.jsx";
+import Register from "./features/auth/pages/Register.jsx";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <Routes>
+      <Route path="/register" element={<Register />} />
+      <Route path="/login" element={<Login />} />
+    </Routes>
+  );
 };
 
 export default App;
---
+++ frontend/src/features/auth/pages/Register.jsx
@@ -0,0 +1,52 @@
+import "../auth.form.scss";
+import { Link } from "react-router";
+
+const Register = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <main>
+      <div className="form-container">
+        <h1>Register</h1>
+        <form onSubmit={handleSubmit}>
+          <div className="input-group">
+            <label htmlFor="username">Username</label>
+            <input
+              type="username"
+              name="username"
+              id="username"
+              placeholder="Enter your username"
+            />
+          </div>
+          <div className="input-group">
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              placeholder="Enter your email"
+            />
+          </div>
+          <div className="input-group">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              name="password"
+              id="password"
+              placeholder="Enter your password"
+            />
+          </div>
+          <button className="button primary-button">Register</button>
+        </form>
+
+        <p>
+          Already have an account? <Link to="/login">Login</Link>
+        </p>
+      </div>
+    </main>
+  );
+};
+
+export default Register;
